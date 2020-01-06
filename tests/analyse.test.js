453f4cfd1088e8cd64569b9ba6b1ab95771361a6
othmane <othman-19@hotmail.fr>
2020-01-06 16:02:05 +0100

    fix error after CR N°1

--- tests/analyse.test.js
+++ tests/analyse.test.js
@@ -1,12 +1,29 @@
 /* eslint-env jest */
 import analyse from '../src/analyse';
-
+const object = analyse([1, 8, 3, 4, 2, 6]);
 test('it returns an object with the average,minimum, maximum and length of the array', () => {
-  const object = analyse([1, 8, 3, 4, 2, 6]);
   expect(object).toEqual({
     average: 4,
     min: 1,
     max: 8,
     length: 6,
+  });
+});
+
+describe('Analyse an array', () => {
+  it('the average', () => {
+    expect(object.average).toEqual(4);
+  });
+
+  it('the minimum', () => {
+    expect(object.min).toEqual(1);
+  });
+
+  it('the maximum', () => {
+    expect(object.max).toEqual(8);
+  });
+
+  it('the length', () => {
+    expect(object.length).toEqual(6);
   });
 });
