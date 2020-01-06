5dc45c135c0af0a1077b7be6cdd628781cc890f7
othmane <othman-19@hotmail.fr>
2020-01-06 12:22:46 +0100

    Test for a caeser shift function

---
+++ tests/caesarShift.test.js
@@ -0,0 +1,17 @@
+import caesarShift from '../src/caesarShift';
+
+test('it shift character by 3', () => {
+  expect(caesarShift('aaa', 3)).toBe('ddd');
+});
+
+test('it wraps character', () => {
+  expect(caesarShift('zzz', 1)).toBe('aaa');
+});
+
+test('it shifts a capital letter to a capital letter', () => {
+  expect(caesarShift('abC', 5)).toBe('fgH');
+});
+
+test('it do not shift special characters or numbers', () => {
+  expect(caesarShift('5abC!', 5)).toBe('5fgH!');
+});
